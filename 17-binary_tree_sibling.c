4af18a9715f0226c60836f09f5541e525859cdb5
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2022-09-22 01:02:13 +0300

    Sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,7 +8,6 @@
  * Return: If node is NULL or there is no sibling - NULL.
  *         Otherwise - a pointer to the sibling.
  */
-
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
@@ -17,4 +16,3 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (node->parent->right);
 	return (node->parent->left);
 }
-Footer
